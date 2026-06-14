b4bd2ff2a27469d6b1f2671db4aedac122fd6692
the-ict <dvltinv@gmail.com>
2026-06-14 09:52:26 +0500

    feat: Add base url correct

--- client/src/shared/config/api/user.request.ts
+++ client/src/shared/config/api/user.request.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "https://digging.davlatbek.uz/api";
 
 export const userRequests = {
   login: async (username: string, password: string) => {
